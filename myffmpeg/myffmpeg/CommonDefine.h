a22c91704ffc11c9cdf11b584000905a22f32680
rox00 <zhangyong198863@163.com>
2016-07-20 22:36:09 +0800

    新增myffmpeg文件！

---
+++ myffmpeg/myffmpeg/CommonDefine.h
@@ -0,0 +1,17 @@
+#pragma once
+
+
+extern "C"
+{
+	#include <libavutil/opt.h>
+	#include<libavcodec/avcodec.h>
+	#include <libavformat/avformat.h>
+	#include <libswscale/swscale.h>
+	#include <libswresample/swresample.h>
+};
+
+class myffAPIs
+{
+	
+
+};
