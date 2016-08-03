00664b757fe7a9efe0be44d48d90a31d78b208a4
rox00 <726372432@qq.com>
2016-08-03 19:03:21 +0800

    测试 构造析构函数 调用虚函数功能！

--- myConsoleTest/myConsoleTest/myConsoleTest.cpp
+++ myConsoleTest/myConsoleTest/myConsoleTest.cpp
@@ -2,10 +2,82 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
+using namespace std;
+/*************构造函数不能为虚函数**********************/
+class MyClass
+{
+public:
+	/*virtual*/ MyClass(){};
+	~MyClass(){};
 
+private:
+
+};
+/************************************************/
+
+/*************构造函数 析构函数 中调用虚函数**********************/
+class Base
+{
+public:
+	Base()
+	{////构造函数中 修改虚表为本类的表
+		Fuction();
+	}
+
+	~Base()
+	{////析构函数中 修改虚表为本类的表
+		Fuction();
+	}
+
+	virtual void Fuction()
+	{
+		cout << "Base::Fuction" << endl;
+	}
+
+	virtual void BaseBase()
+	{
+		cout << "Base::BaseBase" << endl;
+	}
+
+
+};
+
+class A : public Base
+{
+public:
+	A()
+	{
+		Fuction();
+		AAAA();
+	}
+
+	~A()
+	{
+		int a = 0;
+		a = 9;
+		Fuction();
+	}
+
+	virtual void Fuction()
+	{
+		cout << "A::Fuction" << endl;
+	}
+
+	virtual void AAAA()
+	{////如果虚函数不存在于基类中，则不存在于虚表中
+		cout << "A::AA" << endl;
+	}
+};
+/************************************************/
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	//MyClass myclass;
+	Base *pa = new A();
+	pa->Fuction();
+	delete pa;
+	A a;
 	return 0;
 }
 
